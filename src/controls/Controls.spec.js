d4491584533bdfe4c5fc3f41cbf93c925384c2f3
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2019-08-19 15:59:12 -0500

    Completed snapshots for three components

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Controls from './Controls.js';
+
+describe('<Controls />', () => {
+  it('should match new snapshot', () => {
+    const tree = renderer
+      .create(<Controls />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Display from './Display.js';
+
+describe('<Display />', () => {
+  it('should match new snapshot', () => {
+    const DOMtree = renderer
+      .create(<Display />)
+      .toJSON();
+    expect(DOMtree).toMatchSnapshot();
+  });
+});
